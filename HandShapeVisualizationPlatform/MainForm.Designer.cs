3518ca285f0d10176030bfc880c1941cd50d3b7d
nwoedf <tommychan19910123@gmail.com>
2014-06-13 02:25:29 +0800

    Added dummy charts

--- HandShapeVisualizationPlatform/MainForm.Designer.cs
+++ HandShapeVisualizationPlatform/MainForm.Designer.cs
@@ -23,12 +23,168 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.TableLayoutPanel mainTableLayoutPanel;
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+			this.chartHandshape = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			mainTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			mainTableLayoutPanel.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chartHandshape)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// mainTableLayoutPanel
+			// 
+			mainTableLayoutPanel.ColumnCount = 2;
+			mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			mainTableLayoutPanel.Controls.Add(this.chart1, 0, 1);
+			mainTableLayoutPanel.Controls.Add(this.chartHandshape, 0, 1);
+			mainTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			mainTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+			mainTableLayoutPanel.Name = "mainTableLayoutPanel";
+			mainTableLayoutPanel.RowCount = 2;
+			mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			mainTableLayoutPanel.Size = new System.Drawing.Size(1118, 542);
+			mainTableLayoutPanel.TabIndex = 0;
+			// 
+			// chartHandshape
+			// 
+			this.chartHandshape.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(223)))), ((int)(((byte)(193)))));
+			this.chartHandshape.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+			this.chartHandshape.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(64)))), ((int)(((byte)(1)))));
+			this.chartHandshape.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+			this.chartHandshape.BorderlineWidth = 2;
+			this.chartHandshape.BorderSkin.SkinStyle = System.Windows.Forms.DataVisualization.Charting.BorderSkinStyle.Emboss;
+			chartArea2.Area3DStyle.Inclination = 15;
+			chartArea2.Area3DStyle.IsClustered = true;
+			chartArea2.Area3DStyle.IsRightAngleAxes = false;
+			chartArea2.Area3DStyle.Perspective = 10;
+			chartArea2.Area3DStyle.Rotation = 10;
+			chartArea2.Area3DStyle.WallWidth = 0;
+			chartArea2.AxisX.LabelAutoFitMaxFontSize = 8;
+			chartArea2.AxisX.LabelStyle.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold);
+			chartArea2.AxisX.LabelStyle.IsEndLabelVisible = false;
+			chartArea2.AxisX.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			chartArea2.AxisX.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			chartArea2.AxisY.LabelAutoFitMaxFontSize = 8;
+			chartArea2.AxisY.LabelStyle.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold);
+			chartArea2.AxisY.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			chartArea2.AxisY.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			chartArea2.BackColor = System.Drawing.Color.OldLace;
+			chartArea2.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+			chartArea2.BackSecondaryColor = System.Drawing.Color.White;
+			chartArea2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			chartArea2.Name = "Default";
+			chartArea2.ShadowColor = System.Drawing.Color.Transparent;
+			this.chartHandshape.ChartAreas.Add(chartArea2);
+			this.chartHandshape.Dock = System.Windows.Forms.DockStyle.Fill;
+			legend2.BackColor = System.Drawing.Color.Transparent;
+			legend2.Enabled = false;
+			legend2.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold);
+			legend2.IsTextAutoFit = false;
+			legend2.Name = "Default";
+			this.chartHandshape.Legends.Add(legend2);
+			this.chartHandshape.Location = new System.Drawing.Point(562, 274);
+			this.chartHandshape.Name = "chartHandshape";
+			series2.ChartArea = "Default";
+			series2.Legend = "Default";
+			series2.Name = "Series3";
+			series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+			this.chartHandshape.Series.Add(series2);
+			this.chartHandshape.Size = new System.Drawing.Size(553, 265);
+			this.chartHandshape.TabIndex = 3;
+			title2.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold);
+			title2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(59)))), ((int)(((byte)(105)))));
+			title2.Name = "Title1";
+			title2.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			title2.ShadowOffset = 3;
+			title2.Text = "Handshape Probability Distrbution";
+			this.chartHandshape.Titles.Add(title2);
+			// 
+			// chart1
+			// 
+			this.chart1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(223)))), ((int)(((byte)(193)))));
+			this.chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+			this.chart1.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(64)))), ((int)(((byte)(1)))));
+			this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+			this.chart1.BorderlineWidth = 2;
+			this.chart1.BorderSkin.SkinStyle = System.Windows.Forms.DataVisualization.Charting.BorderSkinStyle.Emboss;
+			chartArea1.Area3DStyle.Inclination = 15;
+			chartArea1.Area3DStyle.IsClustered = true;
+			chartArea1.Area3DStyle.IsRightAngleAxes = false;
+			chartArea1.Area3DStyle.Perspective = 10;
+			chartArea1.Area3DStyle.Rotation = 10;
+			chartArea1.Area3DStyle.WallWidth = 0;
+			chartArea1.AxisX.LabelAutoFitMaxFontSize = 8;
+			chartArea1.AxisX.LabelStyle.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold);
+			chartArea1.AxisX.LabelStyle.IsEndLabelVisible = false;
+			chartArea1.AxisX.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			chartArea1.AxisY.LabelAutoFitMaxFontSize = 8;
+			chartArea1.AxisY.LabelStyle.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold);
+			chartArea1.AxisY.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			chartArea1.BackColor = System.Drawing.Color.OldLace;
+			chartArea1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+			chartArea1.BackSecondaryColor = System.Drawing.Color.White;
+			chartArea1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			chartArea1.Name = "Default";
+			chartArea1.ShadowColor = System.Drawing.Color.Transparent;
+			this.chart1.ChartAreas.Add(chartArea1);
+			this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+			legend1.BackColor = System.Drawing.Color.Transparent;
+			legend1.Enabled = false;
+			legend1.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold);
+			legend1.IsTextAutoFit = false;
+			legend1.Name = "Default";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(3, 274);
+			this.chart1.Name = "chart1";
+			series1.ChartArea = "Default";
+			series1.Legend = "Default";
+			series1.Name = "Series3";
+			series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(553, 265);
+			this.chart1.TabIndex = 4;
+			title1.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold);
+			title1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(59)))), ((int)(((byte)(105)))));
+			title1.Name = "Title1";
+			title1.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			title1.ShadowOffset = 3;
+			title1.Text = "Trajectory Probability Distrbution";
+			this.chart1.Titles.Add(title1);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(1118, 542);
+			this.Controls.Add(mainTableLayoutPanel);
+			this.Name = "MainForm";
+			this.Text = "Hand Shape Visualization Plaform";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			mainTableLayoutPanel.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.chartHandshape)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.DataVisualization.Charting.Chart chartHandshape;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
 	}
 }
 
