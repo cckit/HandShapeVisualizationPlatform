f05ac742cf8629caecd66a1dfb1919c7ec840cfe
nwoedf <tommychan19910123@gmail.com>
2014-06-25 04:55:01 +0800

    Initalize datatables

--- HandShapeVisualizationPlatform/CSVDataSetModel.cs
+++ HandShapeVisualizationPlatform/CSVDataSetModel.cs
@@ -33,7 +33,10 @@ namespace HandShapeVisualizationPlatform {
 			set { this.compareDataIndex = value; }
 		}
 
+		private const int HAND_SHAPE_COUNT = 15;
 		private const string RAW_DATA_TABLE_NAME = "Raw Data Table";
+		private const string HAND_SHAPE_DIFF_TABLE_NAME = "Hand Shape Difference";
+		private const string TRAJECTORY_DIFF_TABLE_NAME = "Trajectory Difference";
 
 		public CSVDataSetModel(string pathName) {
 			this.pathName = pathName;
@@ -42,6 +45,30 @@ namespace HandShapeVisualizationPlatform {
 		public override void loadRawData() {
 			DataLoader.loadFromCSV(pathName, false, DataSet.Tables.Add(RAW_DATA_TABLE_NAME));
 			Console.WriteLine("Row: " + DataSet.Tables[RAW_DATA_TABLE_NAME].Rows.Count + "\tCol: " + DataSet.Tables[RAW_DATA_TABLE_NAME].Columns.Count);
+
+			using(DataTable handShapeDataTable = DataSet.Tables.Add(HAND_SHAPE_DIFF_TABLE_NAME)) {
+				handShapeDataTable.Columns.Add("Left Hand");
+				handShapeDataTable.Columns.Add("Right Hand");
+				handShapeDataTable.Columns.Add("Hand Shape");
+
+				for(int i = 1; i <= HAND_SHAPE_COUNT; i++) {
+					handShapeDataTable.Rows.Add(handShapeDataTable.NewRow());
+				}
+			}
+
+			using(DataTable trajectoryDataTable = DataSet.Tables.Add(TRAJECTORY_DIFF_TABLE_NAME)) {
+				trajectoryDataTable.Columns.Add("All");
+				trajectoryDataTable.Columns.Add("1st Half");
+				trajectoryDataTable.Columns.Add("2nd Half");
+				trajectoryDataTable.Columns.Add("1st Quarter");
+				trajectoryDataTable.Columns.Add("2nd Quarter");
+				trajectoryDataTable.Columns.Add("3rd Quarter");
+				trajectoryDataTable.Columns.Add("4th Quarter");
+
+				for(int i = 0; i < 8; i++) {
+					trajectoryDataTable.Rows.Add(trajectoryDataTable.NewRow());
+				}
+			}
 		}
 
 		public override void initDataSet() {
